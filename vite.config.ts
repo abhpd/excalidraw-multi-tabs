a40d68418044e9f92a341d6bbd9955d16df54d85
MontejoJorge <jorgemon.lopez@gmail.com>
2026-08-20 12:30:01 +0200

    remove unused tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import { cloudflare } from '@cloudflare/vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig, loadEnv } from 'vite';
 
@@ -8,7 +7,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
 
   return {
-    plugins: [react(), tailwindcss(), cloudflare()],
+    plugins: [react(), cloudflare()],
     base: env.VITE_BASE_PATH || '/',
     build: {
       outDir: 'dist',
